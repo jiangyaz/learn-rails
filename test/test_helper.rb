871d9094f30f1f27c57212a785772bde92c6e5ae
jiangyaz <jiangyaz@usc.edu>
2014-12-01 16:12:15 -0800

    Ch9 Update, show, delete

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,29 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  	fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  	# Add more helper methods to be used by all tests here...
+  	def log_in_as(user, options = {})
+    	password    = options[:password]    || 'password'
+    	remember_me = options[:remember_me] || '1'
+    	if integration_test?
+      		post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    	else
+      		session[:user_id] = user.id
+    	end
+  	end
+
+  	# Returns true if a test user is logged in.
+  	def is_logged_in?
+    	!session[:user_id].nil?
+  	end
+
+  	# Returns true inside an integration test.
+    def integration_test?
+      	defined?(post_via_redirect)
+    end
+
 end
