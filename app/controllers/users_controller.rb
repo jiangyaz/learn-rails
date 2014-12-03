145949ce184d0ec3d7da0bcb1e12795587cdd3c3
jiangyaz <jiangyaz@usc.edu>
2014-12-02 22:58:55 -0800

    Ch12 Following Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+										  :following, :followers]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user, only: :destroy
 
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@microposts = @user.microposts.paginate(page: params[:page])
 	end
 
 	def new
@@ -40,6 +42,20 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def following 
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.following.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
 	def destroy
 		User.find(params[:id]).destroy
 		flash[:sucess] = "User deleted"
@@ -50,14 +66,6 @@ class UsersController < ApplicationController
 		def user_params
 			params.require(:user).permit(:name, :email, :password, 
 										 :password_confirmation)
-		end
-
-		def logged_in_user
-			unless logged_in?
-				store_location
-				flash[:danger] = "Please log in."
-				redirect_to login_url
-			end
 		end
 
 		def correct_user
